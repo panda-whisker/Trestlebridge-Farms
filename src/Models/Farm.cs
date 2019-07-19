f712f741fd23f093eca9cac858456e368838f495
Dan Storm <stormdanp@gmail.com>
2019-07-19 11:24:55 -0500

    issue ticket 1 completed

--- src/Models/Farm.cs
+++ src/Models/Farm.cs
@@ -30,6 +30,7 @@ namespace Trestlebridge.Models
         public void AddGrazingField (GrazingField field)
         {
             GrazingFields.Add(field);
+            System.Console.WriteLine($"{field} has been added");
         }
 
         public override string ToString()
