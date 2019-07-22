364f321afba7517b4506441b3dd2be6d0999000b
Jason Collum <jasoncollum@gmail.com>
2019-07-22 10:27:50 -0500

    add additional incoming change

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -3,7 +3,6 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
 
-<<<<<<< HEAD
 namespace Trestlebridge.Actions
 {
     public class CreateFacility
@@ -12,16 +11,8 @@ namespace Trestlebridge.Actions
         {
             Console.WriteLine("1. Grazing field");
             Console.WriteLine("2. Plowed field");
-            Console.WriteLine("3. Chicken House");
-=======
-namespace Trestlebridge.Actions {
-    public class CreateFacility {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Grazing field");
-            Console.WriteLine ("2. Plowed field");
-            Console.WriteLine ("3. Duck House");
-            Console.WriteLine ("4. Chicken House");
->>>>>>> master
+            Console.WriteLine("3. Duck House");
+            Console.WriteLine("4. Chicken House");
 
             Console.WriteLine();
             Console.WriteLine("Choose what you want to create");
