d9d23e7ec4c0c15dd333072fcad0b277015e1742
dek-haji <deeeqhaji@gmail.com>
2019-07-23 10:28:41 -0500

    ticket number 9

--- src/Actions/ChooseGrazingField.cs
+++ src/Actions/ChooseGrazingField.cs
@@ -2,8 +2,8 @@ using System;
 using System.Linq;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
+using Trestlebridge.Models.Facilities;
 using Trestlebridge.Models.Animals;
-
 namespace Trestlebridge.Actions
 {
     public class ChooseGrazingField
@@ -11,24 +11,21 @@ namespace Trestlebridge.Actions
         public static void CollectInput(Farm farm, IGrazing animal)
         {
             Console.Clear();
-
-            for (int i = 0; i < farm.GrazingFields.Count; i++)
+         for (int i = 0; i < farm.GrazingFields.Count; i++)
             {
-                Console.WriteLine($"{i + 1}. Grazing Field {farm.GrazingFields[i].id}");
+                if (farm.GrazingFields[i].AnimalCount <= farm.GrazingFields[i].Capacity)
+                {
+                    Console.WriteLine($"{i + 1}. Grazing Field {farm.GrazingFields[i].id} has {farm.GrazingFields[i].AnimalCount} animals");
+                    Console.WriteLine($"Place the animal where?");
+                    Console.Write("> ");
+                    int choice = Int32.Parse(Console.ReadLine());
+                farm.GrazingFields[choice - 1].AddResource(animal);
+                }
+                else { System.Console.WriteLine("This field is full"); }
             }
-
             Console.WriteLine();
-
             // How can I output the type of animal chosen here?
-            Console.WriteLine($"Place the animal where?");
-
-            Console.Write("> ");
-            int choice = Int32.Parse(Console.ReadLine());
-
-            farm.GrazingFields[choice - 1].AddResource(animal);
-
-
-            /*
+           /*
                 Couldn't get this to work. Can you?
                 Stretch goal. Only if the app is fully functional.
              */
--- src/Models/Facilities/GrazingField.cs
+++ src/Models/Facilities/GrazingField.cs
@@ -3,14 +3,12 @@ using System.Text;
 using System.Collections.Generic;
 using Trestlebridge.Interfaces;
 
-
 namespace Trestlebridge.Models.Facilities
 {
     public class GrazingField : IFacility<IGrazing>
     {
-        private int _capacity = 50;
+        public double Capacity { get; } = 5;
         private Guid _id = Guid.NewGuid();
-
         public string id
         {
             get
@@ -19,20 +17,23 @@ namespace Trestlebridge.Models.Facilities
                 return shortId;
             }
         }
-
         private List<IGrazing> _animals = new List<IGrazing>()
         {
-
         };
-
-        public double Capacity
+        public double AnimalCount
         {
             get
             {
-                return _capacity;
+                return _animals.Count;
             }
         }
-
+        // public static double Capacity
+        // {
+        //     get
+        //     {
+        //         return _capacity;
+        //     }
+        // }
         public void AddResource(IGrazing animal)
         {
             // Add animal to List or return user to facility list in terminal
@@ -45,22 +46,17 @@ namespace Trestlebridge.Models.Facilities
                 Console.WriteLine("Press return to choose a different facility");
             }
         }
-
         public void AddResource(List<IGrazing> animals)
         {
             // TODO: implement this...
-
-            // throw new NotImplementedException();
+         // throw new NotImplementedException();
         }
-
         public override string ToString()
         {
             StringBuilder output = new StringBuilder();
             string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
-
             output.Append($"Grazing field ID# {shortId} has {this._animals.Count} animals\n");
             this._animals.ForEach(a => output.Append($"   {a}\n"));
-
             return output.ToString();
         }
     }
